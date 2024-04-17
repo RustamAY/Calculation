e0482f37ff280b422456688eebe34e3d30a881f1
rusyun8 <rustam.yunusov@attolabs.de>
2024-04-17 14:58:26 +0500

    added buttoms

--- src/calculation/Form.java
+++ src/calculation/Form.java
@@ -1,6 +1,7 @@
 package calculation;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Form extends JFrame {
 
@@ -8,6 +9,20 @@ public class Form extends JFrame {
         super("General form");
         super.setBounds(500,500,500, 470);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = super.getContentPane();
+        container.setLayout(new GridLayout(5, 1, 2, 10));
+
+        JButton gehStofe = new JButton("Gehandhabte stofe");
+        JButton gehBrenstofe = new JButton("Gehandhabte Brennstofe");
+        JButton gehTierhaltung = new JButton("Tierhaltung");
+        JButton emission = new JButton("Emissionen");
+
+        container.add(gehStofe);
+        container.add(gehBrenstofe);
+        container.add(gehTierhaltung);
+        container.add(emission);
+
 
     }
 
